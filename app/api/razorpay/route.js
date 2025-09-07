7788b0a84d8c22e6e7aafad52ff851762da71d9c
Aishwary Gupta <74300436+011aishwary@users.noreply.github.com>
2025-09-08 04:40:39 +0530

    Update route.js

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -2,11 +2,17 @@ import { NextResponse } from "next/server";
 import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
 import Payment from "../../../models/Payment";
 import connectDB from "../../../db/connectDB";
+import User from "../../../models/User";
 import Razorpay from "razorpay";
 // import Pay from "@/app/pay/page";
 
 export const POST = async (request) => {
     await connectDB();
+    let u = await User.find({ username: "Aishwary Gupta" })
+        // let user = JSON.parse(JSON.stringify(u));
+    let secret = u[0].razorpaySecret
+    console.log(secret)
+    console.log(u)
     let body = await request.formData()
     body = Object.fromEntries(body);
     // console.log(body)
@@ -18,7 +24,7 @@ export const POST = async (request) => {
     }
 
     // verify the payment
-    let xx=validatePaymentVerification({"order_id":body.razorpay_order_id , "payment_id":body.razorpay_payment_id} , body.razorpay_signature, process.env.NEXT_PUBLIC_RAZORPAY_SECRET_KEY)
+    let xx=validatePaymentVerification({"order_id":body.razorpay_order_id , "payment_id":body.razorpay_payment_id} , body.razorpay_signature, secret)
 
     if (xx) {
         // update payment status in db
