95e5c25bddfc69450d9dbbef0e731f89bc76ed7f
Aishwary Gupta <74300436+011aishwary@users.noreply.github.com>
2025-09-08 03:27:09 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,15 +1,41 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/Chai`, {
-        useNewUrlParser: true,
-      });
-      // console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(`mongodb://localhost:27017/Chai`, {
+      bufferCommands: false, // keep this
+    }).then((mongoose) => mongoose);
+    // console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
+}
 
-  export default connectDB;
+export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable");
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// export async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI, {
+//       bufferCommands: false, // keep this
+//     }).then((mongoose) => mongoose);
+//   }
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
